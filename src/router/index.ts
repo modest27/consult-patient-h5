d73ed159220195d2a35eed235c35e4bf328eaff7
modest_ <1342994266@qq.com>
2023-01-02 19:42:51 +0800

    布局容器layout搭建

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,66 @@
+import { useUserStore } from '@/stores'
 import { createRouter, createWebHistory } from 'vue-router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/login', component: () => import('@/views/Login/index.vue') }
+    {
+      path: '/login',
+      component: () => import('@/views/Login/index.vue'),
+      meta: { title: '登录' }
+    },
+    {
+      path: '/',
+      component: () => import('@/views/Layout/index.vue'),
+      redirect: '/home',
+      children: [
+        {
+          path: '/home',
+          component: () => import('@/views/Home/index.vue'),
+          meta: { title: '首页' }
+        },
+        {
+          path: '/article',
+          component: () => import('@/views/Article/index.vue'),
+          meta: { title: '健康百科' }
+        },
+        {
+          path: '/notify',
+          component: () => import('@/views/Notify/index.vue'),
+          meta: { title: '消息通知' }
+        },
+        {
+          path: '/user',
+          component: () => import('@/views/User/index.vue'),
+          meta: { title: '个人中心' }
+        }
+      ]
+    }
   ]
+})
+
+// 访问权限的控制
+router.beforeEach((to) => {
+  // 如果 return true 或者什么都不写 就是放行
+  // 拦截的话 return '/指定路径' 就是
+  // 需求：当没有登录也就是没有token 并且 访问的不是登录页 就拦截
+
+  NProgress.start()
+
+  // 用户仓库
+  const store = useUserStore()
+  // 不需要登录的页面，白名单
+  const wihteList = ['/login']
+  // 如果没有登录且不在白名单内，去登录
+  if (!store.user?.token && !wihteList.includes(to.path)) return '/login'
+})
+
+router.afterEach((to) => {
+  // 处理标题
+  document.title = `优医问诊-${to.meta.title || ''}`
+  NProgress.done()
 })
 
 export default router
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="layout-page">
+    <!-- 二级路由的出口 article home notify user等组件-->
+    <router-view></router-view>
+    <!-- 底部tab栏 -->
+    <van-tabbar route>
+      <van-tabbar-item replace to="/home"
+        >首页<template #icon="{ active }"
+          ><CpIcon
+            :name="`home-index-${active ? 'active' : 'default'}`"
+          ></CpIcon></template
+      ></van-tabbar-item>
+      <van-tabbar-item replace to="/article"
+        >健康百科<template #icon="{ active }"
+          ><CpIcon
+            :name="`home-article-${active ? 'active' : 'default'}`"
+          ></CpIcon></template
+      ></van-tabbar-item>
+      <van-tabbar-item replace to="/notify"
+        >消息通知<template #icon="{ active }"
+          ><CpIcon
+            :name="`home-notice-${active ? 'active' : 'default'}`"
+          ></CpIcon></template
+      ></van-tabbar-item>
+      <van-tabbar-item replace to="/user"
+        >我的<template #icon="{ active }"
+          ><CpIcon
+            :name="`home-mine-${active ? 'active' : 'default'}`"
+          ></CpIcon></template
+      ></van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout-page {
+  :deep() {
+    .van-tabbar-item {
+      &__icon {
+        font-size: 21px;
+      }
+      &__text {
+        font-size: 11px;
+      }
+      &:not(.van-tabbar-item--active) {
+        color: var(--cp-text3);
+      }
+    }
+  }
+}
+</style>
