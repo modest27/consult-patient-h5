c8369a14d25d1434382b2a2273b5116cc92328fb
modest_ <1342994266@qq.com>
2023-01-04 22:48:54 +0800

    首页模块-知识列表

---
+++ src/views/Home/components/KnowledgeList.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { getKnowledgePage } from '@/services/consult'
+import type {
+  KnowledgeList,
+  KnowledgeParams,
+  KnowledgeType
+} from '@/types/consult'
+import { ref } from 'vue'
+import KnowledgeCard from './KnowledgeCard.vue'
+
+// 父组件传入的列表文章类型
+const props = defineProps<{
+  type: KnowledgeType
+}>()
+
+const loading = ref(false)
+const finished = ref(false)
+
+const list = ref<KnowledgeList>([])
+const params = ref<KnowledgeParams>({
+  type: props.type,
+  current: 1,
+  pageSize: 10
+})
+
+const onLoad = async () => {
+  // 加载更多
+  const res = await getKnowledgePage(params.value)
+  list.value.push(...res.data.rows)
+  if (params.value.current >= res.data.pageTotal) {
+    finished.value = true
+  } else {
+    params.value.current++
+  }
+  loading.value = false
+}
+</script>
+
+<template>
+  <div class="knowledge-list">
+    <van-list
+      v-model:loading="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <knowledge-card
+        v-for="item in list"
+        :key="item.id"
+        :item="item"
+      ></knowledge-card>
+    </van-list>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.knowledge-list {
+  padding: 0 15px;
+}
+</style>
